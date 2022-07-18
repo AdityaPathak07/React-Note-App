6471f8cd6ebce1c162d00839ea8cbbb16c543976
Aditya Pathak <adityaamaniphone@gmail.com>
2022-07-18 13:27:13 +0530

    Updates

---
+++ precache-manifest.179e4fe6562c891ba14ce7416e080b66.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363328b984fd998c8581a67561b7e6bb",
+    "url": "/React-Note-App/index.html"
+  },
+  {
+    "revision": "52e9ebebcab11123ba0d",
+    "url": "/React-Note-App/static/js/2.3782b2ae.chunk.js"
+  },
+  {
+    "revision": "a8f5a56af6cd2d688ef6",
+    "url": "/React-Note-App/static/js/main.8039b88f.chunk.js"
+  },
+  {
+    "revision": "0834e4bd98756d67d293",
+    "url": "/React-Note-App/static/js/runtime-main.ed0daa70.js"
+  }
+]);
